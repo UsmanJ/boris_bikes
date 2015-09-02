4db30eaed86e1cf2477dcb3ebc71c16f43116903
Usman Jamil <usman.jamil@live.co.uk>
2015-09-02 16:50:49 +0100

    post challenge 1 completed

--- setting_up_rspec/spec/docking_station_spec.rb
+++ setting_up_rspec/spec/docking_station_spec.rb
@@ -1,12 +1,19 @@
 require 'docking_station'
-require 'bike'
 
 describe DockingStation do
 	it { is_expected.to respond_to :release_bike}
 
-	it 'releases bike if working' do
+	it 'releases working bikes' do
+		subject.dock Bike.new
 		bike = subject.release_bike
 		expect(bike).to be_working
 	end
 
+	it { is_expected.to respond_to(:dock).with(1).argument }
+
+	describe '#release_bike' do
+		it 'raises an error when there are no bikes available' do
+			expect { subject.release_bike }.to raise_error 'No bikes available'
+		end
+	end
 end
